a667c06294710bd519b1fb56d4dce391232e1e35
mhelluy <matthieu.helluy@gmail.com>
2021-08-23 18:08:05 +0200

    ez

--- install_key.py
+++ install_key.py
@@ -19,51 +19,59 @@ chdir(path[0])
 config = dataload("config")
 
 if len(argv) <= 1:
-    print("You have to specify the public key to add :\nThe command should look like : {} ssh-rsa XXX..XXX user@example.fr".format(argv[0]))
+    print("You have to specify the public key to add :\nThe command should look like : {} \"ssh-rsa XXX..XXX user@example.fr\"".format(argv[0]))
     exit()
 
-while True:
-    print("Which permission file you want to apply to the user ?")
-    permfiles = [".".join(f.split(".")[0:-1]) for f in sorted(listdir("perms"))]
 
-    for i,permf in enumerate(permfiles):
-        print(" [{}] {}".format(i+1,permf))
 
-    try:
-        perm = permfiles[int(input("? (1--{}) ".format(len(permfiles)))) - 1]
-    except IndexError:
-        print("Incorrect ID.\n\n")
-    except ValueError:
-        print("Incorrect ID.\n\n")
-    except EOFError:
-        print()
-        exit()
-    else:
-        break
+permfiles = [".".join(f.split(".")[0:-1]) for f in sorted(listdir("perms"))]
+if len(argv) >= 3 and argv[2] in permfiles:
+    perm = argv[2] 
+else:
+    while True:
+        print("Which permission file you want to apply to the user ?")
 
-while True:
-    print("Which user do you want to assign to this key ?")
-    userfiles = [".".join(f.split(".")[0:-1]) for f in sorted(listdir(ospath.expanduser(config["paths"]["userpath"].replace("$$",path[0]))))]
+        for i,permf in enumerate(permfiles):
+            print(" [{}] {}".format(i+1,permf))
 
-    for i,userf in enumerate(userfiles):
-        print(" [{}] {}".format(i+1,userf))
+        try:
+            perm = permfiles[int(input("? (1--{}) ".format(len(permfiles)))) - 1]
+        except IndexError:
+            print("Incorrect ID.\n\n")
+        except ValueError:
+            print("Incorrect ID.\n\n")
+        except EOFError:
+            print()
+            exit()
+        else:
+            break
 
-    try:
-        user = userfiles[int(input("? (1--{}) ".format(len(userfiles)))) - 1]
-    except IndexError:
-        print("Incorrect ID.\n\n")
-    except ValueError:
-        print("Incorrect ID.\n\n")
-    except EOFError:
-        print()
-        exit()
-    else:
-        break
+userfiles = [".".join(f.split(".")[0:-1]) for f in sorted(listdir(ospath.expanduser(config["paths"]["userpath"].replace("$$",path[0]))))]
+if len(argv) >= 4 and argv[3] in userfiles:
+    user = argv[3]
+else:
+    while True:
+        print("Which user do you want to assign to this key ?")
+
+        for i,userf in enumerate(userfiles):
+            print(" [{}] {}".format(i+1,userf))
+
+        try:
+            user = userfiles[int(input("? (1--{}) ".format(len(userfiles)))) - 1]
+        except IndexError:
+            print("Incorrect ID.\n\n")
+        except ValueError:
+            print("Incorrect ID.\n\n")
+        except EOFError:
+            print()
+            exit()
+        else:
+            break
 
 chdir(ospath.expanduser("~"))
 
 system("mkdir -p .ssh/")
 system("touch .ssh/authorized_keys")
 
-open(".ssh/authorized_keys","a").write('\ncommand="python3 {0}/rshell.py {1} {2}" {3}'.format(path[0],perm,user," ".join(argv[1:])))
+open(".ssh/authorized_keys","a").write('\ncommand="python3 {0}/rshell.py {1} {2}" {3}'.format(path[0],perm,user,argv[1]))
 print("The key has been written is authorized keys.")
